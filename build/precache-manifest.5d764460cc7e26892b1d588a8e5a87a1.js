e7a42bf6ba7127539353945615cc10430e9965e0
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-02-04 21:28:25 -0800

    dpdate deployed

---
+++ build/precache-manifest.5d764460cc7e26892b1d588a8e5a87a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb139a6be300ee9d8a5f",
+    "url": "/Clicky-Game/static/css/main.e3fa4621.chunk.css"
+  },
+  {
+    "revision": "bb139a6be300ee9d8a5f",
+    "url": "/Clicky-Game/static/js/main.bb139a6b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "de77348f747fda230521fe29d0589499",
+    "url": "/Clicky-Game/static/media/main_bg.de77348f.png"
+  },
+  {
+    "revision": "727bd44ccfe006c964793237d181d12f",
+    "url": "/Clicky-Game/index.html"
+  }
+];
